94ebeccd1db349afaa26b45b53dc7253a81c89f1
Edison Kardirsson <ed.kardirsson@hotmail.com>
2025-10-28 12:11:58 +0100

    add: library

---
+++ PoindextersLibrary/Book.cs
@@ -0,0 +1,25 @@
+namespace PoindextersLibrary;
+
+public class Book
+{
+    private int Id { get; }
+    public string Name { get; } = string.Empty;
+    public string Author { get; } = string.Empty;
+    public string Publisher { get; } = string.Empty;
+    public int Year { get; }
+    // ReSharper disable once InconsistentNaming
+    public string ISBN { get; }
+    public bool IsLoaned { get; set; }
+    public DateTime? DueDate { get; set; }
+    
+    private const int MaxLoanDays = 30;
+    
+    public Book(string name, string author, string publisher, int year, string isbn)
+    {
+        Name = name;
+        Author = author;
+        Publisher = publisher;
+        Year = year;
+        ISBN = isbn;
+    }
+}
---
+++ PoindextersLibrary/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PoindextersLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.CursorTop++;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Hello, World!");
+    }
+}
